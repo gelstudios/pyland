3817fc1068bba2da6170cfc6590fdde060df9b9c
Eric Romano <gelstudios@gmail.com>
2013-03-20 15:14:47 -0400

    Revert "templates"
    This reverts commit 21f0090bddca232403b866f6ad802f4a8d6952eb.

--- webapp/app.py
+++ webapp/app.py
@@ -1,26 +1,7 @@
 import bottle
 import pymongo
 import os
-from bottle import route, request, get, post, template
-
-#database names:
-DBNAME = 'tinybbs'
-
-#i think this needs db connection class? lots of repetition going on here
-
-def db_init():
-	try:
-		connection = pymongo.MongoClient('localhost', 27017) # try get db connection
-	except ConnectionFailure:
-		print 'Sorry! We could not connect to the database %s', DBNAME
-	try:
-		connection['test'].db.bbs
-	except:
-		#create a collection called bbs in the database defined above
-		print 'could not access bbs collection in %s', DBNAME
-		pass
-	pass
-
+from bottle import route, request, post, template
 
 @route('/')
 def index():
@@ -29,19 +10,24 @@ def index():
 	except ConnectionFailure:
 		return 'Sorry! We could not connect to the database'
 	db = connection['test'] # get our test db
-	bbs = db.bbs # get our bbs collection
+	bbs = db.bbs # get our bbs colleWe ction
 	#message = bbs.find_one() #get a message
 	docs = bbs.find() #get all the messages!
 	print repr(docs)
 	return template('index', messages=docs) 
+	#content + ['<br><form action="/postmsg" method="get"> Account: <input type="text" name="account"><br>Message: <input type="text" name="message"><br><input type="submit" formmethod="get" formaction="/postmsg" value="post my message"></form>']
+
+#@route('/postmsg', method='POST')
+#fresh
 
 @route('/postmsg')
 #@post('/postmsg')
 def postmsg():
 #	account = request.forms.get('account')
 #	message = request.forms.get('message')
-	#get post data with request.forms.get
-#get 'get parameters' with request.query.get
+	#get post data
+
+#get 'get parameters'
 	account = request.query.get('account')
 	message = request.query.get('message')
 
@@ -53,27 +39,28 @@ def postmsg():
 	db = connection['test'] # get our test db
 	bbs = db.bbs # get our bbs colleWe ction
 	bbs.save(post_data)
-	return template('yay thanks for posting!') + bottle.redirect('/') #add bottle.redirect to root
-
-@get('/login')
-def login_page():
-	return template('login',)
-
-@route('/login', method='POST')
-def auth_user():
-	user = request.forms.get('u')
-	passwd = request.forms.get('p')
+	return 'yay thanks for posting!' #post_data
 
 @route('/users')
+@route('/users/')
+def login():
+	body = 'this is some txt, pretend you see a fake login page'
+	return t2html( body, 'body')
+
 @route('/users/<userid>')
-def user( userid=None ):
+def user( userid ):
 	if userid == None:
-		return bottle.redirect('/')
+		return 'this is the a fancy login page'
 	else:
-		return 'hello ' + userid
+		return t2html( 'hello ' + userid, 'body') + t2html('/message', 'link')
+
+def t2html( text, tag ):
+	if tag == 'link':
+		return '<a href="' + text + '">' + text + '</a>'
+	else:
+		return '<' + tag + '>' + text + '</' + tag + '>'
 
 def main():
-	db_init()
 	bottle.run(host='localhost', port=8080, debug=True, reloader=True)
 	pass
 
