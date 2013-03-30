16f01f70d2245304bfbdad149b36c808e862f8be
Eric Romano <gelstudios@gmail.com>
2013-03-29 23:45:10 -0400

    testing

--- stepper.py
+++ stepper.py
@@ -4,6 +4,6 @@ def stepper(start, step, limit):
 	if start < limit:
 		print start
 		start = start + step
-		return summ(start, step, limit)
+		return stepper(start, step, limit)
 	else:
 		return "finito!"
